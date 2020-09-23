dafb435966ab4fb17684ad3ede0b78763d9bfb49
tesfamariamyebio <65938837+tesfamariamyebio@users.noreply.github.com>
2020-09-23 17:24:11 +0200

    committt first

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
+const Restaurant=require('./models/restaurant');
 
 
 const port = process.env.PORT || 3000;
@@ -15,7 +16,6 @@ app.get('/', (req, res) =>{
     console.log('GET /', req.body)
     res.send('bonjour')
 });
-
 
 app.listen(port, ()=>{
     console.log(`Server started on port : ${port}!`)
---
+++ models/restaurant.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const restaurant=new mongoose.Schema({
+    firtsname:String,
+    surname:String,
+    age:Number,
+    grade:String
+});
+const Restaurant=mongoose.model('Restaurant',restaurantSchema);
+
